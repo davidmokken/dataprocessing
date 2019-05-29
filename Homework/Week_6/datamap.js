4d767be9f50b80b4ce40b0a562c3b5f489e47682
davidmokken <davidmokken@hotmail.com>
2019-05-29 19:57:44 +0200

    Week 6&7

--- Homework/Week_6/datamap.js
+++ Homework/Week_6/datamap.js
@@ -7,20 +7,21 @@ const tip = d3.tip()
   .html(d => `<strong>Province: </strong><span class='details'>${d.properties.name}<br></span><strong>Percentage Gelovigen: </strong><span class='details'>${format(d.total_bel)}%</span>`);
 
 const margin = {top: 0, right: 0, bottom: 0, left: 0};
-const width = 960 - margin.left - margin.right;
+const width = 600 - margin.left - margin.right;
 const height = 500 - margin.top - margin.bottom;
 
 const colorscale = d3.scaleLinear()
   .domain([0, 100])
   .range(["#EFEFFF","#02386F"]);
 
-const svg = d3.select('body')
+const svg = d3.select('#datamap')
   .append('svg')
   .attr('width', width)
   .attr('height', height)
   .append('g')
   .attr('class', 'map');
 
+// These numbers show the Netherlands out of the entire and complete worldmap
 const projection = d3.geoRobinson()
   .scale(8200)
   .center([0, 52])
@@ -48,6 +49,9 @@ function ready(error, data, population) {
     ColorList[key] = colorscale(population[key]['Total']);  
   }
   data.features.forEach(d =>  { d.total_bel = GelovigenProvincie[d.properties.name]});
+ 
+  // Starts shows a bar chart of the verdeling van geloof in the Netherlands as a whole
+  updateBar(population['Nederland'])
 
   svg.append('g')
     .attr('class', 'countries')
@@ -57,8 +61,6 @@ function ready(error, data, population) {
     .append('path')
     .attr('d', path)
     .style('fill', function(d){
-        console.log(d)
-        console.log(ColorList[d.properties.name])
         return ColorList[d.properties.name]
     })
     .attr("class", function(d, i) {
@@ -80,7 +82,17 @@ function ready(error, data, population) {
         d3.select(this)
           .style('opacity', 0.8)
           .style('stroke-width',0.3);
-      });
+      })
+
+      .on("click", function(d){
+        updateBar(population[d.properties.name])
+      })
+  svg.append('text')
+      .attr('x', (width / 2))
+      .attr('y',  (margin.top/2))
+      .attr('text-anchor', 'middle')
+      .text('The Netherlands')
+
 
   svg.append('path')
     .datum(topojson.mesh(data.features, (a, b) => a.id !== b.id))
---
+++ Homework/Week_6/piechart.js
@@ -0,0 +1,132 @@
+// Javascript Piechart
+
+// Constant values
+const width_svg = 700;
+const height_svg = 700;
+const width_pie = 500;
+const height_pie = 500;
+const margin_pie = 40;
+const legendRectSize = 25;
+const legendSpacing = 6;
+
+// The radios of the piechart
+const radius_pie = Math.min(width_pie, height_pie) / 2.5 - margin_pie;
+
+// Set tooltips
+const tip_pie = d3.tip()
+  .attr('class', 'd3-tip')
+  .offset([-10, 0])
+  .html(d => `<strong>Religion: </strong><span class='details'>${d.data.key}<br></span><strong>Percentage: </strong><span class='details'>${format(d.value)}%</span>`);
+
+// Append svg object to the div "pie_chart"
+const svg_pie = d3.select("#pie_chart")
+            .append("svg")
+            .attr("width", width_svg)
+            .attr("height", height_svg)
+            .append("g")
+            .attr("transform", `translate(${width_pie / 2}, ${height_pie / 2})`)
+svg_pie.call(tip_pie);
+    // Sets the color scale
+    const color = d3.scaleOrdinal()
+                    .domain(['Athiest', 'Dutch Reformed', 'Islam', 'Other', 'Protestant', 'Reformed', 'Roman Catholic'])
+                    .range(["#66c2a5","#fc8d62","#8da0cb","#e78ac3","#a6d854","#ffd92f", "#0f60e2"]);
+  
+    // A function that creates/updates the plot for a given province
+    function updateBar(data) {
+        delete data.Total;
+        
+        // Computes the position of each religion on the pie
+        var pie = d3v4.pie()
+                    .value(function(d) {
+                        return d.value;
+                    })
+                    .sort(function(a, b) {
+                        return d3v4.ascending(a.key, b.key);
+                    })
+        
+        var data_ready = pie(d3v4.entries(data))
+
+        data_ready.sort(function(a,b){
+            return a.index - b.index;
+        })
+        
+        // Map to data
+        var u = svg_pie.selectAll("path")
+                    .data(data_ready)
+
+                // Build the pie chart
+                u
+                    .enter()
+                    .append('path')
+
+                    // Mousehover function
+                    .on('mouseover',function(d){
+                        tip_pie.show(d);
+                        d3.select(this)
+                        .style('opacity', 1)
+                        .style('stroke-width', 3);
+                    })
+                    .on('mouseout', function(d){
+                        tip_pie.hide(d);
+                        d3.select(this)
+                        .style('opacity', 0.8)
+                        .style('stroke-width',0.3);
+                    })
+
+                    .merge(u)
+                    .transition()
+                    .duration(1000)
+                    .attr('d', d3v4.arc()
+                        .innerRadius(0)
+                        .outerRadius(radius_pie)
+                    )
+                    .attr('fill', function(d){
+                        return(color(d.index))
+                    })
+                    .attr("class", function(d, i) {
+                        return d.data.key;
+                    })
+                    .attr("stroke", "white")
+                    .style("stroke-width", "2px")
+                    .style("opacity", 1)
+
+                u
+                    .exit()
+                    .remove()
+    }
+
+    svg_pie.append('text')
+    .attr('x', 0)
+    .attr('y', -200)
+    .attr('text-anchor', 'middle')
+    .style('font-size', '20px')
+    .style('font-family', 'Times New Roman')
+    .text('Division of religion in The Netherlands');
+                 
+// Adds a legend                    
+var legend = svg_pie.selectAll(".legend")
+    .data(color.domain())
+    .enter()
+    .append("g")
+    .attr("class", "legend")
+    .attr('transform', function(d, i) {
+        var height = legendRectSize;
+        var x = 8 * legendRectSize;
+        var y = i * height;
+        return 'translate(' + x + ',' + y + ')';
+
+    });
+
+    legend.append('rect')
+        .attr('width', legendRectSize)
+        .attr('height', legendRectSize)
+        .style('fill', color)
+        .style('stroke', color);
+
+    legend.append('text')
+        .attr('x', legendRectSize + legendSpacing)             
+        .attr('y', legendRectSize - legendSpacing)             
+        .text(function(d) { 
+            return d; }); 
+
+
